594bdd2b64066fcdff8b6323c97a0586bf78b920
Cody Turk <gfxop@outlook.com>
2019-03-11 17:28:39 -0500

    add a faq page

--- main.go
+++ main.go
@@ -17,9 +17,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h2>Contact Us</h2>")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type"), "text/html")
+	fmt.Fprint(w, "<h2>FAQ</h2>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
